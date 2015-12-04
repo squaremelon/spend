681b75c2daccdff446e119b3252fd77f2dc9aa87
Angus McMillan <nxtonic@gmail.com>
2015-12-04 19:51:18 +1100

    Added more tasks to Gulpfile - Also setup a playground for store.js - Also added some more dependencies. - Also rearranged bower.json to load in correct order

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,24 @@
 var gulp = require('gulp');
 var inject = require('gulp-inject');
+var less = require('gulp-less');
 var serve = require('gulp-serve');
 var wiredep = require('wiredep').stream;
+
+var path = require('path');
 
 var options = {
   servePort: 8000
 }
 
 var paths = {
-  scripts: ['./static/js/**/*.js'],
-  dependencies: ['./static/bower/']
+  js: ['./static/js/**/*.js'],
+  dependencies: ['./static/bower/'],
+  less: ['./static/less/**/*.less']
 }
+
+gulp.task('default', ['watch', 'serve']);
+
+gulp.task('plug', ['less', 'inject']);
 
 gulp.task('serve', serve({
   root: ['./'],
@@ -25,12 +33,24 @@ gulp.task('wiredep', function () {
     .pipe(gulp.dest('./'));
 });
 
-
-gulp.task('inject', function () {
+gulp.task('inject', ['wiredep'] ,function () {
   var target = gulp.src('./index.html');
   // It's not necessary to read the files (will speed up things), we're only after their paths:
   var sources = gulp.src(['./static/js/**/*.js', './static/css/**/*.css'], {read: false});
 
   return target.pipe(inject(sources, {relative: true}))
     .pipe(gulp.dest('./'));
+});
+
+gulp.task('less', function () {
+  return gulp.src(paths.less)
+    .pipe(less({
+
+    }))
+    .pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.less, ['less', 'inject']);
+  gulp.watch(paths.js, ['inject']);
 });
